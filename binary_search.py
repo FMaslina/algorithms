ca00d798bf07bb8e00da0e5c3512414880a9e0b6
FMaslina <kostyan_20044@mail.ru>
2021-12-24 17:24:52 +0700

    Little update

--- binary_search.py
+++ binary_search.py
@@ -5,12 +5,12 @@ def binary_search(list, item):
     while low <= high:
         mid = (low + high) // 2
         guess = list[mid]
-        if guess == item:
-            return mid
         if guess > item:
             high = mid - 1
-        else:
+        elif guess < item:
             low = mid + 1
+        else:
+            return mid
     return None
 
 
